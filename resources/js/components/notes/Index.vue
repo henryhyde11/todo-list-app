1a210a30b603c2ec5d0e813977dba6e2c8312200
henryhyde11 <serestrepov@gmail.com>
2024-09-30 18:24:47 -0500

    Minor fixes

--- resources/js/components/notes/Index.vue
+++ resources/js/components/notes/Index.vue
@@ -1,48 +1,90 @@
 <template>
-    <div class="w-fit container mx-auto p-8">
-        <div class="flex items-center justify-between gap-4 mb-4">
-            <h1 class="text-3xl font-bold text-center">Resumen de tareas</h1>
+    <div class="flex items-center justify-between gap-4 mb-4">
+        <h1 class="text-3xl font-bold text-center">Resumen de tareas</h1>
 
-            <button
-                type="button"
-                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-                Añadir tarea
-            </button>
+        <button
+            @click="newNote"
+            type="button"
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+            Añadir tarea
+        </button>
+    </div>
+
+    <div
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center items-center bg-blue-100 gap-2"
+    >
+        <div class="max-w-sm p-4 rounded-lg">
+            <div class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg">
+                <div class="flex justify-between">
+                    <div
+                        class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
+                    >
+                        <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
+                        <span>Etiqueta</span>
+                    </div>
+
+                    <DropDownBtn />
+                </div>
+
+                <div>
+                    <h3 class="text-xl font-semibold">
+                        Título: Company website redesign
+                    </h3>
+                    <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                </div>
+
+                <p class="text- text-gray-600">
+                    Descripción: DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit.
+                </p>
+
+                <div>
+                    <img
+                        :src="getImageUrl"
+                        class="w-full h-40 rounded-3xl object-cover"
+                        alt="Café"
+                    />
+                </div>
+
+                <div
+                    class="flex items-center justify-between text-sm text-gray-500"
+                >
+                    <div>
+                        <span>📅Fecha de creación: 7 Sept, 23:00</span>
+                    </div>
+
+                    <div class="text-red-500 font-bold">
+                        <h3>Vencimiento: Two days ago</h3>
+                    </div>
+                </div>
+            </div>
         </div>
 
-        <div
-            class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center items-center bg-blue-100 gap-2"
-        >
-            <div class="max-w-sm p-4 rounded-lg">
+        <div class="max-w-sm p-4 rounded-lg">
+            <div class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg">
                 <div
-                    class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg"
+                    class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
                 >
-                    <div class="flex justify-between">
-                        <div
-                            class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
-                        >
-                            <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
-                            <span>Etiqueta</span>
-                        </div>
+                    <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
+                    <span>Etiqueta</span>
+                </div>
 
-                        <DropDownBtn />
-                    </div>
+                <div>
+                    <h3 class="text-xl font-semibold">
+                        Título: Company website redesign
+                    </h3>
+                    <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                </div>
 
-                    <div>
-                        <h3 class="text-xl font-semibold">
-                            Título: Company website redesign
-                        </h3>
-                        <h3 class="text-lg font-semibold">Usuario: Luis</h3>
-                    </div>
+                <p class="text- text-gray-600">
+                    Descripción: DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit.
+                </p>
 
-                    <p class="text- text-gray-600">
-                        Descripción: DesLorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. DesLorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                    </p>
-
-                    <div>
+                <div>
                         <img
                             :src="getImageUrl"
                             class="w-full h-40 rounded-3xl object-cover"
@@ -50,154 +92,103 @@
                         />
                     </div>
 
-                    <div
-                        class="flex items-center justify-between text-sm text-gray-500"
-                    >
-                        <div>
-                            <span>📅Fecha de creación: 7 Sept, 23:00</span>
-                        </div>
+                <div
+                    class="flex items-center justify-between text-sm text-gray-500"
+                >
+                    <div>
+                        <span>📅Fecha de creación: 7 Sept, 23:00</span>
+                    </div>
 
-                        <div class="text-red-500 font-bold">
-                            <h3>Vencimiento: Two days ago</h3>
-                        </div>
+                    <div class="text-red-500 font-bold">
+                        <h3>Vencimiento: Two days ago</h3>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="max-w-sm p-4 rounded-lg">
+        <div class="max-w-sm p-4 rounded-lg">
+            <div class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg">
                 <div
-                    class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg"
+                    class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
                 >
-                    <div
-                        class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
-                    >
-                        <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
-                        <span>Etiqueta</span>
-                    </div>
+                    <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
+                    <span>Etiqueta</span>
+                </div>
 
+                <div>
+                    <h3 class="text-xl font-semibold">
+                        Título: Company website redesign
+                    </h3>
+                    <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                </div>
+
+                <p class="text- text-gray-600">
+                    Descripción: DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit.
+                </p>
+
+                <div>
+                    <img
+                        :src="getImageUrl"
+                        class="w-full h-40 rounded-3xl object-cover"
+                        alt="Café"
+                    />
+                </div>
+
+                <div
+                    class="flex items-center justify-between text-sm text-gray-500"
+                >
                     <div>
-                        <h3 class="text-xl font-semibold">
-                            Título: Company website redesign
-                        </h3>
-                        <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                        <span>📅Fecha de creación: 7 Sept, 23:00</span>
                     </div>
 
-                    <p class="text- text-gray-600">
-                        Descripción: DesLorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. DesLorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                    </p>
-
-                    <!-- <div>
-                        <img
-                            :src="getImageUrl"
-                            class="w-full h-40 rounded-3xl object-cover"
-                            alt="Café"
-                        />
-                    </div> -->
-
-                    <div
-                        class="flex items-center justify-between text-sm text-gray-500"
-                    >
-                        <div>
-                            <span>📅Fecha de creación: 7 Sept, 23:00</span>
-                        </div>
-
-                        <div class="text-red-500 font-bold">
-                            <h3>Vencimiento: Two days ago</h3>
-                        </div>
+                    <div class="text-red-500 font-bold">
+                        <h3>Vencimiento: Two days ago</h3>
                     </div>
                 </div>
             </div>
+        </div>
 
-            <div class="max-w-sm p-4 rounded-lg">
+        <div class="max-w-sm p-4 rounded-lg">
+            <div class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg">
                 <div
-                    class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg"
+                    class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
                 >
-                    <div
-                        class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
-                    >
-                        <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
-                        <span>Etiqueta</span>
-                    </div>
-
-                    <div>
-                        <h3 class="text-xl font-semibold">
-                            Título: Company website redesign
-                        </h3>
-                        <h3 class="text-lg font-semibold">Usuario: Luis</h3>
-                    </div>
-
-                    <p class="text- text-gray-600">
-                        Descripción: DesLorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. DesLorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                    </p>
-
-                    <div>
-                        <img
-                            :src="getImageUrl"
-                            class="w-full h-40 rounded-3xl object-cover"
-                            alt="Café"
-                        />
-                    </div>
-
-                    <div
-                        class="flex items-center justify-between text-sm text-gray-500"
-                    >
-                        <div>
-                            <span>📅Fecha de creación: 7 Sept, 23:00</span>
-                        </div>
-
-                        <div class="text-red-500 font-bold">
-                            <h3>Vencimiento: Two days ago</h3>
-                        </div>
-                    </div>
+                    <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
+                    <span>Etiqueta</span>
                 </div>
-            </div>
 
-            <div class="max-w-sm p-4 rounded-lg">
+                <div>
+                    <h3 class="text-xl font-semibold">
+                        Título: Company website redesign
+                    </h3>
+                    <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                </div>
+
+                <p class="text- text-gray-600">
+                    Descripción: DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. DesLorem ipsum dolor sit amet, consectetur
+                    adipiscing elit.
+                </p>
+
+                <div>
+                    <img
+                        :src="getImageUrl"
+                        class="w-full h-40 rounded-3xl object-cover"
+                        alt="Café"
+                    />
+                </div>
+
                 <div
-                    class="flex flex-col gap-5 bg-white p-6 rounded-lg shadow-lg"
+                    class="flex items-center justify-between text-sm text-gray-500"
                 >
-                    <div
-                        class="w-fit border p-1 border-blue-500 rounded-lg flex items-center justify-between gap-1"
-                    >
-                        <div class="w-4 h-4 bg-blue-500 rounded-full"></div>
-                        <span>Etiqueta</span>
-                    </div>
-
                     <div>
-                        <h3 class="text-xl font-semibold">
-                            Título: Company website redesign
-                        </h3>
-                        <h3 class="text-lg font-semibold">Usuario: Luis</h3>
+                        <span>📅Fecha de creación: 7 Sept, 23:00</span>
                     </div>
 
-                    <p class="text- text-gray-600">
-                        Descripción: DesLorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. DesLorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
-                    </p>
-
-                    <div>
-                        <img
-                            :src="getImageUrl"
-                            class="w-full h-40 rounded-3xl object-cover"
-                            alt="Café"
-                        />
-                    </div>
-
-                    <div
-                        class="flex items-center justify-between text-sm text-gray-500"
-                    >
-                        <div>
-                            <span>📅Fecha de creación: 7 Sept, 23:00</span>
-                        </div>
-
-                        <div class="text-red-500 font-bold">
-                            <h3>Vencimiento: Two days ago</h3>
-                        </div>
+                    <div class="text-red-500 font-bold">
+                        <h3>Vencimiento: Two days ago</h3>
                     </div>
                 </div>
             </div>
@@ -205,21 +196,18 @@
     </div>
 </template>
 
-<script>
+<script setup>
 import DropDownBtn from "../DropDownBtn.vue";
+import { useRouter } from "vue-router";
+import { ref, computed } from "vue";
 
-export default {
-    data() {
-        return {
-            imageUrl: "/imagenes/cafe-img.jpg",
-        };
-    },
-    computed: {
-        getImageUrl() {
-            return this.imageUrl;
-        },
-    },
-    name: "ParentComponent",
-    components: { DropDownBtn },
+const imageUrl = ref("/imagenes/cafe-img.jpg");
+
+const getImageUrl = computed(() => imageUrl.value);
+
+const router = useRouter();
+
+const newNote = () => {
+  router.push('/notes/create');
 };
 </script>
