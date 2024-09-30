c3664171d1788282110cca99991271068944fde6
henryhyde11 <serestrepov@gmail.com>
2024-09-29 19:49:05 -0500

    Añadir modelo Note

---
+++ database/migrations/2024_09_30_002038_create_notes_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 124);
+            $table->string('image')->nullable();
+            $table->timestamps();
+            $table->date('expiration_at')->nullable();
+            $table->foreignId('user_id');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('notes');
+    }
+};
