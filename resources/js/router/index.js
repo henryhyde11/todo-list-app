d41f02f684a62534901df9085050b7ff7909e139
henryhyde11 <serestrepov@gmail.com>
2024-10-01 17:59:06 -0500

    Añadir notificaciones

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,24 +1,35 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import noteIndex from '../components/notes/Index.vue'
-import notFound from '../components/NotFound.vue'
+import noteIndex from "../components/notes/Index.vue";
+import notFound from "../components/NotFound.vue";
+import noteForm from "../components/notes/Form.vue";
 
 const routes = [
     {
-        path: '/',
-        name: 'notes.index',
-        component: noteIndex
+        path: "/",
+        name: "notes.index",
+        component: noteIndex,
     },
     {
-        path: '/:pathMatch(.*)*',
-        name: 'notfound',
-        component: notFound
-    }
-]
+        path: "/notes/create",
+        name: "notes.create",
+        component: noteForm,
+    },
+    {
+        path: "/notes/:id/edit",
+        name: "notes.edit",
+        component: noteForm,
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: notFound,
+    },
+];
 
-const router =  createRouter({
+const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
-export default router
+export default router;
