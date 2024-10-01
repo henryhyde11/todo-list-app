475ba39e3a169966e33e06245b40e62b02d2672c
henryhyde11 <serestrepov@gmail.com>
2024-10-01 00:39:56 -0500

    Modificar formulario

--- resources/js/components/notes/Form.vue
+++ resources/js/components/notes/Form.vue
@@ -1,12 +1,12 @@
 <template>
-    <h1 class="text-3xl font-bold text-center">Crear nueva tarea</h1>
+    <h1 class="text-3xl font-bold text-center mb-5">Crear nueva tarea</h1>
 
-    <form class="max-w-md mx-auto">
+    <section class="max-w-md mx-auto">
         <!-- Title -->
         <div class="relative z-0 w-full mb-5 group">
             <input
+                v-model="form.title"
                 type="text"
-                name="title"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
@@ -21,8 +21,8 @@
         <!-- Description -->
         <div class="relative z-0 w-full mb-5 group">
             <input
+                v-model="form.description"
                 type="text"
-                name="description"
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
@@ -35,162 +35,141 @@
         </div>
 
         <!-- Fecha de inicio -->
-        <div
-            id="date-range-picker"
-            date-rangepicker
-            class="flex items-center mb-5"
-        >
-            <div class="relative">
-                <div
-                    class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
-                >
-                    <img src="/public/icons/calendario.svg" alt="calendario" />
-                </div>
-                <input
-                    id="datepicker-range-start"
-                    name="started_at"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-                    placeholder="Fecha de inicio"
-                />
-            </div>
+        <div class="flex items-center mb-5">
+            <input
+                v-model="form.started_at"
+                type="date"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+                placeholder="Fecha de inicio"
+            />
+
             <span class="mx-4 text-gray-500"></span>
 
-            <!-- Fecha de vencimiento -->
-            <div class="relative">
-                <div
-                    class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
-                >
-                    <img src="/public/icons/calendario.svg" alt="calendario" />
-                </div>
-                <input
-                    id="datepicker-range-end"
-                    name="finished_at"
-                    type="text"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
-                    placeholder="Fecha de vencimiento"
-                />
-            </div>
+        <!-- Fecha de vencimiento -->
+
+        <input
+                v-model="form.finished_at"
+                type="date"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5"
+                placeholder="Fecha de vencimiento"
+            />
         </div>
 
         <!-- Imagen -->
-        <div class="flex items-center justify-center w-full mb-5">
-            <label
-                for="dropzone-file"
-                class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-10"
-            >
-                <div
-                    class="flex flex-col items-center justify-center pt-5 pb-6"
-                >
-                    <img src="/public/icons/upload.svg" alt="" />
-                    <p class="mb-2 text-sm text-gray-500">
-                        <span class="font-semibold"
-                            >Haz click para subir la imagen</span
-                        >
-                        o arrástrala y suéltala
-                    </p>
-                    <p class="text-xs text-gray-500">
-                        SVG, PNG, JPG or GIF (MAX. 800x400px)
-                    </p>
-                </div>
-                <input
-                    id="dropzone-file"
-                    name="image_uri"
-                    type="file"
-                    class="hidden"
-                />
-            </label>
+        <h3 class="text-lg font-semibold mb-4">
+            Seleccione una imagen (opcional):
+        </h3>
+        <div
+            class="w-full flex flex-col justify-center items-center gap-5 mb-5"
+        >
+            <input
+                @change="handleFileChange"
+                id="dropzone-file"
+                type="file"
+                class="w-full"
+            />
+            <img
+                :src="getImage()"
+                alt="Imagen nota"
+                class="w-32 h-32 rounded-3xl object-cover"
+            />
         </div>
 
         <!-- Etiqueta -->
-        <div class="flex justify-between mb-5">
-            <button
-                id="dropdownCheckboxButton"
-                data-dropdown-toggle="dropdownDefaultCheckbox"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
-                type="button"
-            >
-                Selecciona una etiqueta
-                <svg
-                    class="w-2.5 h-2.5 ms-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                >
-                    <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m1 1 4 4 4-4"
+        <h3 class="text-lg font-semibold mb-4">Seleccione una etiqueta:</h3>
+
+        <div class="mb-5">
+            <div class="flex flex-col space-y-2">
+                <label class="inline-flex items-center">
+                    <input
+                        type="radio"
+                        value="social"
+                        v-model="form.category"
+                        class="peer"
                     />
-                </svg>
-            </button>
+                    <div class="flex items-center">
+                        <span class="ml-2">Social</span>
+                    </div>
+                </label>
 
-            <div
-                id="dropdownDefaultCheckbox"
-                class="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow"
-            >
-                <ul
-                    class="p-3 space-y-3 text-sm text-gray-700"
-                    aria-labelledby="dropdownCheckboxButton"
-                >
-                    <li>
-                        <div class="flex items-center">
-                            <input
-                                id="checkbox-item-1"
-                                type="checkbox"
-                                value=""
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-                            />
-                            <label
-                                for="checkbox-item-1"
-                                class="ms-2 text-sm font-medium text-gray-900"
-                                >Trabajo</label
-                            >
-                        </div>
-                    </li>
-                    <li>
-                        <div class="flex items-center">
-                            <input
-                                id="checkbox-item-2"
-                                type="checkbox"
-                                value=""
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-                            />
-                            <label
-                                for="checkbox-item-2"
-                                class="ms-2 text-sm font-medium text-gray-900"
-                                >Social</label
-                            >
-                        </div>
-                    </li>
+                <label class="inline-flex items-center">
+                    <input
+                        type="radio"
+                        value="estudio"
+                        v-model="form.category"
+                        class="peer"
+                    />
+                    <div class="flex items-center">
+                        <span class="ml-2">Estudio</span>
+                    </div>
+                </label>
 
-                    <li>
-                        <div class="flex items-center">
-                            <input
-                                id="checkbox-item-3"
-                                type="checkbox"
-                                value=""
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
-                            />
-                            <label
-                                for="checkbox-item-3"
-                                class="ms-2 text-sm font-medium text-gray-900"
-                                >Estudio</label
-                            >
-                        </div>
-                    </li>
-                </ul>
+                <label class="inline-flex items-center">
+                    <input
+                        type="radio"
+                        value="trabajo"
+                        v-model="form.category"
+                        class="peer"
+                    />
+                    <div class="flex items-center">
+                        <span class="ml-2">Trabajo</span>
+                    </div>
+                </label>
             </div>
-
-            <button
-                type="submit"
-                class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            >
-                Crear nota
-            </button>
         </div>
-    </form>
+
+        <button
+            @click="handleSave"
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+            Crear nota
+        </button>
+    </section>
 </template>
+
+<script setup>
+import { reactive } from "vue";
+
+const form = reactive({
+    title: "",
+    description: "",
+    started_at: "",
+    finished_at: "",
+    image: "",
+    category: "",
+});
+
+const handleFileChange = (e) => {
+    let file = e.target.files[0];
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        form.image = reader.result;
+    };
+
+    reader.readAsDataURL(file);
+};
+
+const getImage = () => {
+    let image = "/icons/upload.svg";
+
+    if (form.image && form.image.indexOf("base64") != -1) {
+        image = form.image;
+    } else if (form.image) {
+        image = "/imagenes" + form.image;
+    }
+
+    return image;
+};
+
+const handleSave = () => {
+    axios
+        .post("/api/notes", form)
+        .then((response) => {
+            console.log(response.data); // Para verificar si los datos se están enviando correctamente
+        })
+        .catch((error) => {
+            console.error(error.response.data); // Para depurar el error
+        });
+};
+</script>
