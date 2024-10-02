1440e551168a16b1a71a8c65b000ad128c990210
henryhyde11 <serestrepov@gmail.com>
2024-10-02 11:36:20 -0500

    Añadir route link

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -10,7 +10,7 @@
                     <h1
                         class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
                     >
-                        Iniciar sesión
+                        Crea una cuenta
                     </h1>
                     <form
                         @submit.prevent="handleSubmit"
@@ -18,16 +18,31 @@
                     >
                         <div>
                             <label
+                                for="name"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >Nombre</label
+                            >
+                            <input
+                                v-model="form.name"
+                                type="name"
+                                name="name"
+                                id="name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            />
+                        </div>
+
+                        <div>
+                            <label
                                 for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                                >Tu email</label
+                                >Email</label
                             >
                             <input
                                 v-model="form.email"
                                 type="email"
                                 name="email"
                                 id="email"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             />
                         </div>
 
@@ -42,7 +57,22 @@
                                 type="password"
                                 name="password"
                                 id="password"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            />
+                        </div>
+
+                        <div>
+                            <label
+                                for="confirm-password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >Confirma tu contraseña</label
+                            >
+                            <input
+                                v-model="form.password_confirmation"
+                                type="password"
+                                name="confirm-password"
+                                id="confirm-password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             />
                         </div>
 
@@ -50,17 +80,18 @@
                             type="submit"
                             class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
-                            Iniciar sesión
+                            Crear cuenta
                         </button>
-
                         <p
                             class="text-sm font-light text-gray-500 dark:text-gray-400"
                         >
-                            ¿No tienen una cuenta?<a
-                                href="#"
-                                class="font-medium text-blue-600 hover:underline dark:text-blue-500"
-                                >Registrate</a
-                            >
+                            ¿Ya tienes una cuenta?
+
+                            <RouterLink
+                            to="/login"
+                            class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                            >Inicia aquí</RouterLink
+                        >
                         </p>
                     </form>
                 </div>
@@ -71,15 +102,18 @@
 
 <script setup>
 import { reactive } from "vue";
+import { useRouter } from "vue-router";
 
 const form = reactive({
+    name: "",
     email: "",
     password: "",
+    password_confirmation: "",
 });
 
 const handleSubmit = () => {
-    axios.post("/login", form).then(() => {
-        windows.location.href = "/welcome";
+    axios.post("/register", form).then(() => {
+        router.push('/');
     });
 };
 </script>
