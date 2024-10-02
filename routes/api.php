130b7ecce72a499e85863ab4435cef3993d91831
henryhyde11 <serestrepov@gmail.com>
2024-10-01 19:34:31 -0500

    Añadir ruta para eliminar nota

--- routes/api.php
+++ routes/api.php
@@ -14,17 +14,4 @@ Route::post('/notes', [NoteController::class, 'store']);
 Route::get('/notes', [NoteController::class, 'index']);
 Route::get('/notes/{note}/edit', [NoteController::class, 'edit']);
 Route::put('/notes/{note}', [NoteController::class, 'update']);
-
-// Route::middleware('auth:sanctum')->group(function () {
-//     Route::get('/notes', [NoteController::class, 'index']);
-//     Route::post('/notes', [NoteController::class, 'store']);
-//     Route::get('/notes/{id}', [NoteController::class, 'show']);
-//     Route::put('/notes/{id}', [NoteController::class, 'update']);
-//     Route::delete('/notes/{id}', [NoteController::class, 'destroy']);
-
-//     Route::get('/categories', [CategoryController::class, 'index']);
-//     Route::post('/categories', [CategoryController::class, 'store']);
-//     Route::get('/categories/{id}', [CategoryController::class, 'show']);
-//     Route::put('/categories/{id}', [CategoryController::class, 'update']);
-//     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-// });
+Route::delete('/notes/{note}', [NoteController::class, 'destroy']);
