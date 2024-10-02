b15c1a08a8fbc4728399c34cbc40c54c84bc38e2
henryhyde11 <serestrepov@gmail.com>
2024-10-02 11:27:52 -0500

    Añadir ruta home

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,31 +1,36 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store";
 
 import noteIndex from "../components/notes/Index.vue";
 import notFound from "../components/NotFound.vue";
 import noteForm from "../components/notes/Form.vue";
-import login from "../pages/auth/Login.vue"
-import register from "../pages/auth/Login.vue"
+import login from "../pages/auth/Login.vue";
+import register from "../pages/auth/Register.vue";
+import home from "../pages/Home.vue";
 
 const routes = [
     {
         path: "/",
         name: "notes.index",
         component: noteIndex,
+        meta: { requiresAuth: true },
     },
     {
         path: "/notes/create",
         name: "notes.create",
         component: noteForm,
+        meta: { requiresAuth: true },
     },
     {
         path: "/notes/:id/edit",
         name: "notes.edit",
         component: noteForm,
+        meta: { requiresAuth: true },
     },
     {
-        path: "/:pathMatch(.*)*",
-        name: "notfound",
-        component: notFound,
+        path: "/home",
+        name: "home",
+        component: home,
     },
     {
         path: "/login",
@@ -37,11 +42,29 @@ const routes = [
         name: "auth.register",
         component: register,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: notFound,
+    },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+        next({ name: "home" });
+    } else if (
+        (to.name === "auth.login" || to.name === "auth.register") &&
+        store.getters.isAuthenticated
+    ) {
+        next({ name: "notes.index" }); // Redirigir a home si ya está autenticado
+    } else {
+        next();
+    }
 });
 
 export default router;
