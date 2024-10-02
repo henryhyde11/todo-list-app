de3f1d9c6fecffd4446fbd15851d6e172cb78b89
henryhyde11 <serestrepov@gmail.com>
2024-10-02 06:21:11 -0500

    Registrar componentes Login y Register

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,28 @@
-import './bootstrap';
-import { createApp } from 'vue'
-import App from './Components/App.vue'
-import router from './router'
-import Swal from 'sweetalert2'
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./Components/App.vue";
+import router from "./router";
+import Swal from "sweetalert2";
+import Login from './pages/auth/Login.vue';
+import Register from './pages/auth/Register.vue';
 
-window.Swal = Swal
+window.Swal = Swal;
 
 const toast = Swal.mixin({
     toast: true,
-    position: 'top-end',
+    position: "top-end",
     showConfirmButton: false,
     timer: 3000,
-    timerProgressBar: true
-})
+    timerProgressBar: true,
+});
 
-window.toast = toast
+window.toast = toast;
 
 const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
+app.use(router);
+
+app.component('Login', Login);
+app.component('Register', Register);
+
+app.mount("#app");
